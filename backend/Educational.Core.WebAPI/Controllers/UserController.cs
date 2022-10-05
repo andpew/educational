eda29ee7b03682cc27ffeebf3dc48a73be5f64bd
andpew <edarkmail@gmail.com>
2022-10-05 14:03:28 +0300

    Added UserController

---
+++ backend/Educational.Core.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Educational.Core.BLL.Services.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Educational.Core.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[AllowAnonymous]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("/all")]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        return Ok(await _userService.GetAllUsers());
+    }
+}
