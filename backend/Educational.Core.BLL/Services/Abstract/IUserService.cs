ac526c5a9a23fcf15594afbad197333269c8dbfa
andpew <edarkmail@gmail.com>
2022-10-05 14:02:40 +0300

    Added BLL project with UserService

---
+++ backend/Educational.Core.BLL/Services/Abstract/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using Educational.Core.DAL.Entities;
+
+namespace Educational.Core.BLL.Services.Abstract;
+
+public interface IUserService
+{
+    Task<IEnumerable<User>> GetAllUsers();
+}
---
+++ backend/Educational.Core.BLL/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using Educational.Core.BLL.Services.Abstract;
+using Educational.Core.DAL;
+using Educational.Core.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Educational.Core.BLL.Services;
+
+public class UserService : IUserService
+{
+    private readonly DataContext _db;
+    public UserService(DataContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<IEnumerable<User>> GetAllUsers()
+    {
+        return await _db.Users.ToListAsync();
+    }
+}
