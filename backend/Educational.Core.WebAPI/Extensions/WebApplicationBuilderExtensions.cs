c55fc1cec3090a0dacec2ba61762dca384f5ef27
andpew <edarkmail@gmail.com>
2022-10-05 14:04:36 +0300

    Added WebApplicationBuilder extensions With adding PostgreSQL db and using UserService

---
+++ backend/Educational.Core.WebAPI/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Educational.Core.BLL.Services;
+using Educational.Core.BLL.Services.Abstract;
+using Educational.Core.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Educational.Core.WebAPI.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer().AddSwaggerGen();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<DataContext>(o =>
+        {
+            o.UseNpgsql(builder.Configuration.GetConnectionString("Educational"))
+                .EnableDetailedErrors();
+        });
+
+        builder.Services.AddMvc();
+
+        builder.Services.AddScoped<IUserService, UserService>();
+    }
+}
