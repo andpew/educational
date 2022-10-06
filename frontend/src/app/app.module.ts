114ffa76d8b453766094bdef3e83d799d3c1c60e
andpew <edarkmail@gmail.com>
2022-10-06 16:33:07 +0300

    Added toastr for notifications

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,14 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    ToastrModule.forRoot({
+      progressBar: true,
+      countDuplicates: true,
+      preventDuplicates: true,
+      resetTimeoutOnDuplicate: true,
+      timeOut: 2500
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
