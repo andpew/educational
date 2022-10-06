19cda1ff91492bbc7e1aebd1f78e670f05f9ed1e
andpew <edarkmail@gmail.com>
2022-10-06 22:43:04 +0300

    Added NotFoundException

---
+++ backend/Educational.Core.BLL/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Educational.Core.BLL.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string entityName) : base($"Entity {entityName} Not Found")
+    {
+
+    }
+}
