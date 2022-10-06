827229c0639354896163f3391c6b65ed9f9b8380
andpew <edarkmail@gmail.com>
2022-10-06 16:32:31 +0300

    Added custom error handler And removed error interceptor

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { BaseComponent } from './base/base.component';
-
-
-
+import { CustomErrorHandler } from './handlers/error.handler';
 
 @NgModule({
   declarations: [BaseComponent],
   imports: [SharedModule],
+  providers: [{ provide: ErrorHandler, useClass: CustomErrorHandler }]
 })
 export class CoreModule { }
