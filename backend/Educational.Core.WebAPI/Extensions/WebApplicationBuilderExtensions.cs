9e40fb4567801bc48045b160cef2b25ea7744e1c
andpew <edarkmail@gmail.com>
2022-10-06 21:28:52 +0300

    Added backend logger

--- backend/Educational.Core.WebAPI/Extensions/WebApplicationBuilderExtensions.cs
+++ backend/Educational.Core.WebAPI/Extensions/WebApplicationBuilderExtensions.cs
@@ -2,6 +2,10 @@
 using Educational.Core.BLL.Services.Abstract;
 using Educational.Core.DAL;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using ILogger = Serilog.ILogger;
 
 namespace Educational.Core.WebAPI.Extensions;
 
@@ -9,16 +13,41 @@ public static class WebApplicationBuilderExtensions
 {
     public static void ConfigureServices(this WebApplicationBuilder builder)
     {
+        AddLogger(builder);
+        AddDatabase(builder);
+
         builder.Services.AddEndpointsApiExplorer().AddSwaggerGen();
         builder.Services.AddSwaggerGen();
+
+        builder.Services.AddMvc();
+
+        builder.Services.AddScoped<IUserService, UserService>();
+    }
+
+    private static void AddLogger(WebApplicationBuilder builder)
+    {
+        builder.Logging.ClearProviders();
+        ILogger logger = new LoggerConfiguration()
+            .WriteTo.Console(
+                restrictedToMinimumLevel: LogEventLevel.Information
+            )
+            .WriteTo.File(
+                formatter: new CompactJsonFormatter(),
+                path: "Logs/log-errors.json",
+                restrictedToMinimumLevel: LogEventLevel.Error,
+                rollingInterval: RollingInterval.Day
+            )
+            .CreateLogger();
+        builder.Logging.AddSerilog(logger);
+        builder.Services.AddSingleton(logger);
+    }
+
+    private static void AddDatabase(WebApplicationBuilder builder)
+    {
         builder.Services.AddDbContext<DataContext>(o =>
         {
             o.UseNpgsql(builder.Configuration.GetConnectionString("Educational"))
                 .EnableDetailedErrors();
         });
-
-        builder.Services.AddMvc();
-
-        builder.Services.AddScoped<IUserService, UserService>();
     }
 }
