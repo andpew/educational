8061807b0514e9f7d3a6f58c6df1a6f56e0d6ad9
andpew <edarkmail@gmail.com>
2022-10-06 17:04:58 +0300

    Used interceptors

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,11 +1,29 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { BaseComponent } from './base/base.component';
 import { CustomErrorHandler } from './handlers/error.handler';
+import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [BaseComponent],
   imports: [SharedModule],
-  providers: [{ provide: ErrorHandler, useClass: CustomErrorHandler }]
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: CustomErrorHandler
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    }
+  ]
 })
 export class CoreModule { }
