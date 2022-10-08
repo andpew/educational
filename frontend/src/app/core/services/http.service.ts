07a001e6123524f5a31b6b9beca47b54695d5f08
andpew <edarkmail@gmail.com>
2022-10-08 18:25:56 +0300

    Removed unused getHeaders()

--- frontend/src/app/core/services/http.service.ts
+++ frontend/src/app/core/services/http.service.ts
@@ -8,13 +8,8 @@ import { environment } from 'src/environments/environment';
 })
 export class HttpService {
   public baseUrl: string = environment.apiUrl;
-  public headers = new HttpHeaders();
 
   constructor(private http: HttpClient) { }
-
-  public getHeaders(): HttpHeaders {
-    return this.headers;
-  }
 
   public getRequest<T>(url: string): Observable<T> {
     return this.http.get<T>(this.buildUrl(url));
@@ -32,24 +27,26 @@ export class HttpService {
     return this.http.delete<T>(this.buildUrl(url));
   }
 
+  // Http params are also known as query strings
   public getFullRequest<T>(url: string, httpParams?: HttpParams): Observable<HttpResponse<T>> {
     return this.http.get<T>(this.buildUrl(url), {
-      observe: 'response', headers: this.getHeaders(),
+      observe: 'response',
       params: httpParams
     });
   }
 
   public postFullRequest<T>(url: string, payload: object): Observable<HttpResponse<T>> {
-    return this.http.post<T>(this.buildUrl(url), payload, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.post<T>(this.buildUrl(url), payload, { observe: 'response' });
   }
 
   public putFullRequest<T>(url: string, payload?: object): Observable<HttpResponse<T>> {
-    return this.http.put<T>(this.buildUrl(url), payload, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.put<T>(this.buildUrl(url), payload, { observe: 'response' });
   }
 
+  // Http params are also known as query strings
   public deleteFullRequest<T>(url: string, httpParams?: HttpParams): Observable<HttpResponse<T>> {
     return this.http.delete<T>(this.buildUrl(url), {
-      headers: this.getHeaders(), observe: 'response',
+      observe: 'response',
       params: httpParams
     });
   }
