3c0b938b9882c4e6c96d1e65b76c2ff4c72a0b22
andpew <edarkmail@gmail.com>
2022-10-08 11:49:45 +0300

    Added user password and migration for it

--- backend/Educational.Core.DAL/Entities/User.cs
+++ backend/Educational.Core.DAL/Entities/User.cs
@@ -5,4 +5,7 @@ namespace Educational.Core.DAL.Entities;
 public class User : BaseEntity
 {
     public string UserName { get; set; } = null!;
+    public byte[] PasswordHash { get; set; } = new byte[32];
+    public byte[] PasswordSalt { get; set; } = new byte[32];
+    public DateTime? VerifiedAt { get; set; }
 }
---
+++ backend/Educational.Core.DAL/Migrations/20221008084824_AddedUserPassword.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Educational.Core.DAL.Migrations
+{
+    public partial class AddedUserPassword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerifiedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerifiedAt",
+                table: "Users");
+        }
+    }
+}
