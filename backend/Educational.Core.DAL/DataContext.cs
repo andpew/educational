a3cb7eb13e240eec2dac594bff0cd1ad310ba420
andpew <edarkmail@gmail.com>
2022-10-08 13:52:54 +0300

    Made user email and username unique

--- backend/Educational.Core.DAL/DataContext.cs
+++ backend/Educational.Core.DAL/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Educational.Core.DAL.Entities;
+using Educational.Core.DAL.EntityConfiguration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Educational.Core.DAL;
@@ -6,6 +7,11 @@ namespace Educational.Core.DAL;
 public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserConfiguration).Assembly);
+    }
 
     public DbSet<User> Users { get; set; } = null!;
 }
---
+++ backend/Educational.Core.DAL/EntityConfiguration/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Educational.Core.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Educational.Core.DAL.EntityConfiguration;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasIndex(x => x.UserName)
+            .IsUnique();
+
+        builder.HasIndex(x => x.Email)
+            .IsUnique();
+    }
+}
