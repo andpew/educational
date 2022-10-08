7698a59e5a1e588a8ef7aeabb4e04c5c55744320
andpew <edarkmail@gmail.com>
2022-10-08 13:02:47 +0300

    Added User email

--- backend/Educational.Core.DAL/Entities/User.cs
+++ backend/Educational.Core.DAL/Entities/User.cs
@@ -5,6 +5,7 @@ namespace Educational.Core.DAL.Entities;
 public sealed class User : BaseEntity
 {
     public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public byte[] PasswordHash { get; set; } = new byte[32];
     public byte[] PasswordSalt { get; set; } = new byte[32];
     public DateTime? VerifiedAt { get; set; }
