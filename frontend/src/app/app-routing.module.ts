ff372a5a3dad0653bdce0cf0a64c8df2feb3a645
andpew <edarkmail@gmail.com>
2022-10-08 18:51:42 +0300

    Added basic routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./views/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule),
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
