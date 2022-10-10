72f59a7b9bbbb926b6b54c43685be04f32409957
andpew <edarkmail@gmail.com>
2022-10-10 22:27:38 +0300

    Added token entity

---
+++ backend/Educational.Core.Common/DTO/Auth/AuthTokenDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace Educational.Core.Common.DTO.Auth;
+
+public class AuthTokenDTO
+{
+    public string Token { get; set; } = null!;
+}
--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { UserLogin } from '../models/user-login.model';
-import { UserRegister } from '../models/user-register.model';
+import { UserLogin } from '../models/auth/user-login.model';
+import { UserRegister } from '../models/auth/user-register.model';
 import { User } from '../models/user.model';
 import { HttpService } from './http.service';
 
