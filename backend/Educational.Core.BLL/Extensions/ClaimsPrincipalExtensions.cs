389181e3b35d445ecd1318642b765d4c3354f8c0
andpew <edarkmail@gmail.com>
2022-10-12 12:19:22 +0300

    Added extensions for getting entities from claims

---
+++ backend/Educational.Core.BLL/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Educational.Core.BLL.Helpers;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Educational.Core.BLL.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static int? GetUserIdFromPrincipal(this ClaimsPrincipal principal)
+    {
+        if (int.TryParse(principal.FindFirstValue(JwtRegisteredClaimNames.Sub), out var result))
+        {
+            return result;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public static DateTime? GetExpirationDateFromPrincipal(this ClaimsPrincipal principal)
+    {
+        if (long.TryParse(principal.FindFirstValue(JwtRegisteredClaimNames.Exp), out var result))
+        {
+            return DateTimeHelper.FromUnixEpochDate(result);
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public static string? GetTokenIdFromPrincipal(this ClaimsPrincipal principal)
+    {
+        return principal.FindFirstValue(JwtRegisteredClaimNames.Jti);
+    }
+}
