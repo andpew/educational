cf63693be8fb82cfc6dcc7ed72921cce973cc9f0
andpew <edarkmail@gmail.com>
2022-10-12 12:14:08 +0300

    Added JwtSecurityExtensions for ability of stringifying token

---
+++ backend/Educational.Core.BLL/Extensions/JwtSecurityTokenExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace Educational.Core.BLL.Extensions;
+
+public static class JwtSecurityTokenExtensions
+{
+    private static readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler;
+
+    static JwtSecurityTokenExtensions()
+    {
+        _jwtSecurityTokenHandler = new();
+    }
+
+    public static string Stringify(this JwtSecurityToken token)
+    {
+        return _jwtSecurityTokenHandler.WriteToken(token);
+    }
+}
