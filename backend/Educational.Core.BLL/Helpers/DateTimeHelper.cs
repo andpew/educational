d7cc31e56a3d7f2dcf58444cc4f2100683cb706e
andpew <edarkmail@gmail.com>
2022-10-12 12:13:11 +0300

    Added datetime helper

---
+++ backend/Educational.Core.BLL/Helpers/DateTimeHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Educational.Core.BLL.Helpers;
+
+public static class DateTimeHelper
+{
+    public static long ToUnixEpochDate(DateTime date)
+    {
+        return new DateTimeOffset(date.ToUniversalTime()).ToUnixTimeSeconds();
+    }
+
+    public static DateTime FromUnixEpochDate(long date)
+    {
+        return DateTimeOffset.FromUnixTimeSeconds(date).UtcDateTime;
+    }
+}
