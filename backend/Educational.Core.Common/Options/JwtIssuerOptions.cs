b073ff19f1b532eb80aef07972a334f186627529
andpew <edarkmail@gmail.com>
2022-10-12 12:15:50 +0300

    Changed ValidFor from int to timespan

--- backend/Educational.Core.Common/Options/JwtIssuerOptions.cs
+++ backend/Educational.Core.Common/Options/JwtIssuerOptions.cs
@@ -3,7 +3,7 @@
 public class JwtIssuerOptions
 {
     public string Key { get; set; } = null!;
-    public int ValidFor { get; set; }
+    public TimeSpan ValidFor { get; set; }
     public string Issuer { get; set; } = null!;
     public string Audience { get; set; } = null!;
 }
