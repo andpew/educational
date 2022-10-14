7e1c7088df540201248405d71580e348ba450e99
andpew <edarkmail@gmail.com>
2022-10-14 14:50:45 +0300

    Added login guard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
+import { LoginGuard } from './core/guards/login.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./views/auth/auth.module').then(m => m.AuthModule),
+    canActivate: [LoginGuard],
+    canActivateChild: [LoginGuard]
   },
   {
     path: 'home',
