1f0c4453cf7bc058199ed8ec3a26caf8d5f2c528
andpew <edarkmail@gmail.com>
2022-10-14 15:12:59 +0300

    Now using 2 spaces instead of 4

--- frontend/src/app/core/handlers/error.handler.ts
+++ frontend/src/app/core/handlers/error.handler.ts
@@ -1,25 +1,25 @@
-import { HttpErrorResponse, HttpEventType, HttpStatusCode } from "@angular/common/http";
+import { HttpErrorResponse, HttpStatusCode } from "@angular/common/http";
 import { ErrorHandler, Inject, Injectable, Injector } from "@angular/core";
 import { ToastrService } from "ngx-toastr";
 
 @Injectable()
 export class CustomErrorHandler implements ErrorHandler {
 
-    constructor(@Inject(Injector) private readonly injector: Injector) { }
+  constructor(@Inject(Injector) private readonly injector: Injector) { }
 
-    private get toastrService() {
-        return this.injector.get(ToastrService);
+  private get toastrService() {
+    return this.injector.get(ToastrService);
+  }
+
+  handleError(error: Error): void {
+    if (error instanceof HttpErrorResponse) {
+      switch (error.status) {
+        case HttpStatusCode.Unauthorized:
+          break;
+      }
     }
 
-    handleError(error: Error): void {
-        if (error instanceof HttpErrorResponse) {
-            switch (error.status) {
-                case HttpStatusCode.Unauthorized:
-                    break;
-            }
-        }
-
-        console.error(error);
-        this.toastrService.error(error.message, error.name);
-    }
+    console.error(error);
+    this.toastrService.error(error.message, error.name);
+  }
 }
